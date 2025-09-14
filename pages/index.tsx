739edecf5e481a5b970f98ba77591a2c869e919d
grouk-eth <grouk.eth@gmail.com>
2025-09-14 16:37:53 +0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import GROUKSite from "../components/GROUKSite";
+
+export default function Home() {
+return (
+<>
+<Head>
+<title>GROUK — Trader‑First, Scarce, Unstoppable</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" href="/favicon.svg" />
+</Head>
+<GROUKSite />
+</>
+);
+}
